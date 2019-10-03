0646aefd508a111b975f6c6379927debc6e9b093
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2019-10-03 10:57:37 +0200

    Setup skeleton project
    Setup skeleton project so that we can configure the build pipeline

---
+++ ToGit/ToGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
