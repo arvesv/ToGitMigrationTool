c3c20f6ee892965421112f780a35874c6489510b
Arve Svendsen <657470+arvesv@users.noreply.github.com>
2019-10-03 14:06:16 +0200

    Compact directory hierarchy

---
+++ ToGit/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CommandLine;
+
+namespace ToGit
+{
+
+
+    class Program
+    {
+        public class Options
+        {
+            [Option('s', "setupfile", Required = true, HelpText ="Path to setup file")]
+            public string SetupFile { get; set; }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
